88e88de3c28c67d344f73dcd133702b49db01284
Kabasele243 <fkabasele7@gmail.com>
2019-09-19 19:18:55 -0400

    refactoring our routes

--- app.js
+++ app.js
@@ -3,13 +3,11 @@ const express = require('express');
 
 const app = express();
 app.use(express.json());
-// app.get('/', (req, res) => {
-//     res.status(200).json({ message: 'Hello from the server!', app: 'natours' });
-// });
+
 
 const courses = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/courses.json`));
 
-app.get('/api/v1/courses', (req, res) => {
+const getAllCourses = (req, res) => {
     res.status(200).json({
         status: 'success',
         results: courses.length,
@@ -17,9 +15,31 @@ app.get('/api/v1/courses', (req, res) => {
             courses
         }
     })
-})
+};
 
-app.post('/api/v1/courses', (req, res) => {
+const getCourse = (req, res) => {
+    /* To get number out of a string */
+    const id = req.params.id * 1;
+    const course = courses.find(el => el.id === id);
+
+    // if (id > courses.length) {
+    if (!course) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        })
+    }
+
+    /* Find us a callback function to loops through the object and find value equals to the req.params */
+    res.status(200).json({
+        status: 'success',
+        data: {
+            course
+        }
+    })
+    console.log(course);
+}
+const createCourse = (req, res) => {
 
     const newId = courses[courses.length - 1].id + 1;
     const newCourse = Object.assign({ id: newId }, req.body);
@@ -36,36 +56,9 @@ app.post('/api/v1/courses', (req, res) => {
     })
 
 
-})
+};
 
-app.get('/api/v1/courses/:id', (req, res) => {
-
-
-    /* To get number out of a string */
-    const id = req.params.id * 1;
-    const course = courses.find(el => el.id === id);
-
-    // if (id > courses.length) {
-    if (!course) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        })
-    }
-
-    /* Find us a callback function to loops through the object and find value equals to the req.params */
-
-
-    res.status(200).json({
-        status: 'success',
-        data: {
-            course
-        }
-    })
-    console.log(course);
-})
-
-app.patch('/api/v1/courses/:id', (req, res) => {
+const updateCourse = (req, res) => {
     console.log(res.params.id)
     if (res.params.id * 1 > courses.length) {
         return res.status(404).json({
@@ -79,8 +72,9 @@ app.patch('/api/v1/courses/:id', (req, res) => {
             course: '<Update tour here ...'
         }
     })
-})
-app.delete('/api/v1/courses/:id', (req, res) => {
+};
+
+const deleteCourse = (req, res) => {
 
 
     if (res.params.id * 1 > courses.length) {
@@ -93,7 +87,25 @@ app.delete('/api/v1/courses/:id', (req, res) => {
         status: 'success',
         data: null
     })
-})
+}
+
+// app.get('/api/v1/courses', getAllCourses);
+// app.post('/api/v1/courses', createCourse);
+// app.get('/api/v1/courses/:id', getCourse);
+// app.patch('/api/v1/courses/:id', updateCourse);
+// app.delete('/api/v1/courses/:id', deleteCourse);
+
+app
+    .route('/api/v1/courses')
+    .get(getAllCourses)
+    .post(createCourse);
+
+
+app
+    .route('/api/v1/courses/:id')
+    .get(getCourse)
+    .patch(updateCourse)
+    .delete(deleteCourse);
 
 const port = 3500;
 app.listen(port, () => {
