81102b3f44dcd9a4735bfe9528ef7be215869e06
Kabasele243 <fkabasele7@gmail.com>
2019-09-24 23:05:09 -0400

    Add global error handling

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const courseRouter = require('./routes/courseRoutes');
 const studentRouter = require('./routes/studentRoutes');
 
@@ -13,12 +15,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use(express.json());
-
-
-app.use((req, res, next) => {
-    console.log('Hello from the middleware');
-    next();
-});
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
@@ -30,4 +27,10 @@ app.use((req, res, next) => {
 app.use('/api/v1/courses', courseRouter);
 app.use('/api/v1/students', studentRouter);
 
+
+app.all('*', (req, res, next) => {
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+})
+
+app.use(globalErrorHandler)
 module.exports = app;
