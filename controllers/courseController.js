2908477407eb3d03787b2dd70f8c94d8adff1c3a
Kabasele243 <fkabasele7@gmail.com>
2019-09-25 12:20:25 -0400

    Catching errors in async function

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,127 +1,93 @@
-const Course = require('./../models/courseModel')
-const APIFeatures = require('./../utils/apiFeatures')
-
-exports.getAllCourses = async (req, res) => {
-    try {
-
-        const features = new APIFeatures(Course.find(), req.query).filter().sort().sort().limitFields().paginate();
-
-        const courses = await features.query;
-
-        res.status(200).json({
-            status: 'success',
-            requestedAt: req.requestTime,
-            results: courses.length,
-            data: {
-                courses
-            }
-        });
-    } catch (err) {
-        res.status(404).json({
-            status: 'failure',
-            message: err
-        })
-    }
-};
-
-exports.getCourse = async (req, res) => {
-
-    try {
-        const course = await Course.findById(req.params.id);
-        res.status(200).json({
-            status: 'success',
-            data: {
-                course
-            }
-        });
-    } catch (err) {
-        res.status(404).json({
-            status: 'fail',
-            message: err
-        })
-
-    }
+const Course = require('./../models/courseModel');
+const APIFeatures = require('./../utils/apiFeatures');
+const catchAsync = require('./../utils/catchAsync');
 
 
-};
+exports.getAllCourses = catchAsync(async (req, res) => {
 
-exports.createCourse = async (req, res) => {
-    try {
+    const features = new APIFeatures(Course.find(), req.query).filter().sort().sort().limitFields().paginate();
 
-        const newCourse = await Course.create(req.body)
+    const courses = await features.query;
 
-        res.status(201).json({
-            status: 'success',
-            data: {
-                course: newCourse
-            }
-        });
-    } catch (err) {
-        res.status(400).json({
-            status: 'fail',
-            message: 'Invalid data sent'
-        })
-
-    }
-};
-
-exports.updateCourse = async (req, res) => {
-    try {
-        const course = await Course.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-        res.status(200).json({
-            status: 'success',
-            data: {
-                course
-            }
-        })
-    } catch (err) {
-        res.status(400).json({
-            status: 'fail',
-            message: err
-        })
-    }
-};
-
-exports.deleteCourse = async (req, res) => {
-    try {
-        await Course.findByIdAndDelete(req.params.id);
-        res.status(204).json({
-            status: 'success',
-            data: null
-        })
-    } catch (err) {
-        res.status(400).json({
-            status: 'fail',
-            message: err
-        })
-    }
-}
+    res.status(200).json({
+        status: 'success',
+        requestedAt: req.requestTime,
+        results: courses.length,
+        data: {
+            courses
+        }
+    });
 
 
-exports.getCourseStats = async (req, res) => {
-    try {
-        const stats = await Course.aggregate([
-            {
-                $group: {
-                    _id: null,
-                    overview: {
-                        student: { $gte: 30 }
-                    }
+});
+
+
+exports.getCourse = catchAsync(async (req, res) => {
+    const course = await Course.findById(req.params.id);
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            course
+        }
+    })
+});
+
+
+
+exports.createCourse = catchAsync(async (req, res, next) => {
+
+    const newCourse = await Course.create(req.body)
+
+    res.status(201).json({
+        status: 'success',
+        data: {
+            course: newCourse
+        }
+    });
+});
+
+exports.updateCourse = catchAsync(async (req, res) => {
+
+    const course = await Course.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+    res.status(200).json({
+        status: 'success',
+        data: {
+            course
+        }
+    })
+});
+
+exports.deleteCourse = catchAsync(async (req, res) => {
+
+    await Course.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+        status: 'success',
+        data: null
+    })
+
+})
+
+
+exports.getCourseStats = catchAsync(async (req, res) => {
+
+    const stats = await Course.aggregate([
+        {
+            $group: {
+                _id: null,
+                overview: {
+                    student: { $gte: 30 }
                 }
             }
-        ]);
+        }
+    ]);
 
-        res.status(200).json({
-            status: 'success',
-            data: {
-                stats
-            }
-        });
+    res.status(200).json({
+        status: 'success',
+        data: {
+            stats
+        }
+    });
 
-    } catch (err) {
-        res.status(400).json({
-            status: 'fail',
-            message: err
-        })
-    }
-}
+
+})
