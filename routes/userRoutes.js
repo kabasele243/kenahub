b863bdf34c5bd9444316f810024f6e6d9c3869f5
Kabasele243 <fkabasele7@gmail.com>
2019-09-26 17:05:43 -0400

    Password reset functionality

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,7 +5,10 @@ const authController = require('./../controllers/authController');
 const router = express.Router();
 
 router.post('/signup', authController.signup);
-router.post('/login', authController.login)
+router.post('/login', authController.login);
+
+router.post('/forgotPassword', authController.forgotPassword);
+router.post('/resetPassword', authController.resetPassword);
 
 router
     .route('/')
